555f81a8aea86323b5b45119b237c35b64702e73
homfunctor <thehundredvisions@proton.me>
2026-04-16 12:44:14 -0500

    ok

--- modules/home/imageEditing.nix
+++ modules/home/imageEditing.nix
@@ -6,10 +6,7 @@
   ...
 }:
 flake.lib.reqHTier config "work" {
-  home.packages = with pkgs; [
-    gimp3
-    kdePackages.kolourpaint
-  ];
+  home.packages = with pkgs; [gimp3];
 
   xdg.mimeApps = {
     associations.added."image/x-dds" = "gimp.desktop";
