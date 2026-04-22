9cfed2434238e98486a97368fc9fa560ff87dae2
homfunctor <thehundredvisions@proton.me>
2026-04-22 17:36:42 -0500

    winetricks trickery

--- modules/home/gaming.nix
+++ modules/home/gaming.nix
@@ -1,16 +1,25 @@
 # imported by tier-personal
-{pkgs, ...}: {
-  home.packages = with pkgs; [
-    hashdeep
-    heroic
-    ludusavi
-    lutris
-    protonplus
-    steamcmd
-    umu-launcher
-    wineWow64Packages.fonts
-    wineWow64Packages.waylandFull
-    winetricks
-    xrandr
-  ];
+{pkgs, ...}: let
+  # to get around the issue:
+  #   warning: wine cmd.exe /c echo '%AppData%' returned empty string, error message ""
+  winetricked = pkgs.writeShellScriptBin "winetricks" ''
+    export PATH="${pkgs.wineWow64Packages.stable}/bin:${pkgs.winetricks}/bin:$PATH"
+    exec ${pkgs.winetricks}/bin/winetricks "$@"
+  '';
+in {
+  home = {
+    packages = with pkgs; [
+      hashdeep
+      heroic
+      ludusavi
+      lutris
+      protonplus
+      steamcmd
+      umu-launcher
+      wineWow64Packages.fonts
+      wineWow64Packages.waylandFull
+      winetricked
+      xrandr
+    ];
+  };
 }
