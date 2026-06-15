01539458059bcdca3c8567ffc216b114b9d444f6
homfunctor <thehundredvisions@proton.me>
2026-06-15 17:58:39 -0500

    ok

--- flake.nix
+++ flake.nix
@@ -74,7 +74,10 @@
       url = "github:kaylorben/nixcord";
     };
 
-    nixvim.url = "github:nix-community/nixvim";
+    nixvim = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/nixvim";
+    };
 
     rust-overlay = {
       inputs.nixpkgs.follows = "nixpkgs";
