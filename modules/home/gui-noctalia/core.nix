cb774f3f39db3a13a2cb60c538105de0786279e4
homfunctor <thehundredvisions@proton.me>
2026-06-23 04:38:10 -0500

    slight change to how i like it

--- modules/home/gui-noctalia/core.nix
+++ modules/home/gui-noctalia/core.nix
@@ -18,76 +18,77 @@ in
   flake.lib.reqHTier config "work" {
     home.opts.gui.noctalia.enabled = true;
 
-    programs = {
-      # niri starts noctalia
-      niri = mkIf nixOpts.wm.niri.enabled {
-        settings.spawn-at-startup = [
-          {
-            command = [
-              noctExe
-              "--no-duplicate"
-            ];
-          }
-          {
-            command = noctCmd "volume muteInput";
-          }
-        ];
-      };
+    programs =
+      {
+        noctalia-shell = {
+          enable = true;
+          package = nixOpts.gui.noctalia.pkg;
 
-      noctalia-shell = {
-        enable = true;
-        package = nixOpts.gui.noctalia.pkg;
+          settings =
+            {
+              general = {
+                allowPanelsOnScreenWithoutBar = true;
+                avatarImage = "${inputs.utumno}/assets/${hmOpts.userName}.png";
+                dimmerOpacity = 0;
+                forceBlackScreenCorners = false;
+                showScreenCorners = false;
 
-        settings =
-          {
-            general = {
-              allowPanelsOnScreenWithoutBar = true;
-              avatarImage = "${inputs.utumno}/assets/${hmOpts.userName}.png";
-              dimmerOpacity = 0;
-              forceBlackScreenCorners = false;
-              showScreenCorners = false;
+                boxRadiusRatio = 0;
+                iRadiusRatio = 0;
+                radiusRatio = 0;
+                scaleRatio = 1;
+                screenRadiusRatio = 0;
 
-              boxRadiusRatio = 0;
-              iRadiusRatio = 0;
-              radiusRatio = 0;
-              scaleRatio = 1;
-              screenRadiusRatio = 0;
+                animationDisabled = false;
+                animationSpeed = 1;
+                enableShadows = true;
+                shadowDirection = "bottom_right";
+                shadowOffsetX = 2;
+                shadowOffsetY = 3;
+                telemetryEnabled = false;
+              };
 
-              animationDisabled = false;
-              animationSpeed = 1;
-              enableShadows = true;
-              shadowDirection = "bottom_right";
-              shadowOffsetX = 2;
-              shadowOffsetY = 3;
-              telemetryEnabled = false;
-            };
+              audio = {
+                externalMixer = "${lib.getExe pkgs.pwvucontrol}";
+                preferredPlayer = hmOpts.apps.audio.exe;
+              };
 
-            audio = {
-              externalMixer = "${lib.getExe pkgs.pwvucontrol}";
-              preferredPlayer = hmOpts.apps.audio.exe;
-            };
+              dock.enabled = false;
 
-            dock.enabled = false;
+              sessionMenu.showHeader = false;
 
-            sessionMenu.showHeader = false;
-
-            ui = with config.stylix; {
-              boxBorderEnabled = true;
-              fontDefault = fonts.sansSerif.name;
-              fontDefaultScale = 1.25;
-              fontFixed = fonts.monospace.name;
-              fontFixedScale = 1.25;
-              panelBackgroundOpacity = 1;
-              panelsAttachedToBar = true;
-              scrollbarAlwaysVisible = true;
-              settingsPanelAttachToBar = false;
-              settingsPanelMode = "window";
-              settingsPanelSideBarCardStyle = false;
-              tooltipsEnabled = true;
-              translucentWidgets = false;
-            };
-          }
-          // nixOpts.gui.noctalia.misc;
-      };
-    };
+              ui = with config.stylix; {
+                boxBorderEnabled = true;
+                fontDefault = fonts.sansSerif.name;
+                fontDefaultScale = 1.25;
+                fontFixed = fonts.monospace.name;
+                fontFixedScale = 1.25;
+                panelBackgroundOpacity = 1;
+                panelsAttachedToBar = true;
+                scrollbarAlwaysVisible = true;
+                settingsPanelAttachToBar = false;
+                settingsPanelMode = "window";
+                settingsPanelSideBarCardStyle = false;
+                tooltipsEnabled = true;
+                translucentWidgets = false;
+              };
+            }
+            // nixOpts.gui.noctalia.misc;
+        };
+      }
+      // (mkIf nixOpts.wm.niri.enabled {
+        niri = {
+          settings.spawn-at-startup = [
+            {
+              command = [
+                noctExe
+                "--no-duplicate"
+              ];
+            }
+            {
+              command = noctCmd "volume muteInput";
+            }
+          ];
+        };
+      });
   }
